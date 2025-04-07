088d7067b70b7ac0258317c8139a4e7cf3a6a50a
bkhebert <bondman2004tippy@gmail.com>
2025-04-07 17:50:11 -0500

    ADD html

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import Scene from './Scene';
+// import Scene from './Scene';
 
 const App = () => {
 
@@ -9,9 +9,9 @@ const App = () => {
         <div className="p-10 bg-red-500 text-white text-2xl">
         If this text is red, Tailwind is working... frfrfr
       </div>
-      <div style={{ width: '100vw', height: '100vw' }}>
-      <Scene/>
-      </div>
+      {/* <div style={{ width: '100vw', height: '100vw' }}>
+ 
+      </div> */}
       </div>
 
     )
