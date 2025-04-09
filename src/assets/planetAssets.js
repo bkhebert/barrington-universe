b3073a23a59bc236a2755b9c584e39ddadab9206
bkhebert <bondman2004tippy@gmail.com>
2025-04-09 03:15:28 -0500

    ADD planet assets file

---
+++ src/assets/planetAssets.js
@@ -0,0 +1,59 @@
+import skewedjs from "../assets/skewedjs.png";
+import angular from "../assets/skewedlogos/angular.png"
+import css from "../assets/skewedlogos/css.png"
+import digitalocean from "../assets/skewedlogos/digitalocean.png"
+import expressjs from "../assets/skewedlogos/express-js.png"
+import github from "../assets/skewedlogos/github.png"
+import googlecloud from "../assets/skewedlogos/googlecloud.png"
+import HTML5 from "../assets/skewedlogos/HTML5.png"
+import linux from "../assets/skewedlogos/linux.png"
+import magicui from "../assets/skewedlogos/magicui.png"
+import materialui from "../assets/skewedlogos/materialui.png"
+import mongodb from "../assets/skewedlogos/mongodb.png"
+import mysql from "../assets/skewedlogos/mysql.png"
+import nginx from "../assets/skewedlogos/nginx.png"
+import node from "../assets/skewedlogos/node.png"
+import npm from "../assets/skewedlogos/npm.png"
+import pixi from "../assets/skewedlogos/pixi.png"
+import postgresql from "../assets/skewedlogos/postgresql.png"
+import prettier from "../assets/skewedlogos/prettier.png"
+import react from "../assets/skewedlogos/react.png"
+import shadcn from "../assets/skewedlogos/shadcn.png"
+import sql from "../assets/skewedlogos/SQL.png"
+import sqlite from "../assets/skewedlogos/sqlite.png"
+import tailwind from "../assets/skewedlogos/tailwind.png"
+import threejs from "../assets/skewedlogos/threejs.png"
+import typescript from "../assets/skewedlogos/typescript.png"
+import webpack from "../assets/skewedlogos/webpack.png"
+
+export default [
+    { size: 2, xp: 1/27, distance: 30, speed: 0.002, color: "#f4ec0f",  image: skewedjs, },
+    { size: 2, xp: 2/27, distance: 30, speed: 0.002, color: "#eda100",  image: angular, },
+    { size: 2, xp: 3/27, distance: 30, speed: 0.002, color: "#0fd6f4",  image: css, },
+    { size: 2, xp: 4/27, distance: 30, speed: 0.002, color: "#005fff",  image: digitalocean, },
+    { size: 2, xp: 5/27, distance: 30, speed: 0.002, color: "#25f900",  image: expressjs, },
+    { size: 2, xp: 6/27, distance: 30, speed: 0.002, color: "#ffffff",  image: github, },
+    { size: 2, xp: 7/27, distance: 30, speed: 0.002, color: "#000000",  image: googlecloud, },
+    { size: 2, xp: 8/27, distance: 30, speed: 0.002, color: "#f4810f",  image: HTML5, },
+    { size: 2, xp: 9/27, distance: 30, speed: 0.002, color: "#504e42",  image: linux, },
+    { size: 2, xp: 10/27, distance: 30, speed: 0.002, color: "#6800ff",  image: magicui, },
+    { size: 2, xp: 11/27, distance: 30, speed: 0.002, color: "#ffffff",  image: materialui, },
+    { size: 2, xp: 12/27, distance: 30, speed: 0.002, color: "#60ef61",  image: mongodb, },
+    { size: 2, xp: 13/27, distance: 30, speed: 0.002, color: "#97e7ff",  image: mysql, },
+    { size: 2, xp: 14/27, distance: 30, speed: 0.002, color: "#005e19",  image: nginx, },
+    { size: 2, xp: 15/27, distance: 30, speed: 0.002, color: "#ffffff",  image: node, },
+    { size: 2, xp: 16/27, distance: 30, speed: 0.002, color: "#c20000",  image: npm, },
+    { size: 2, xp: 17/27, distance: 30, speed: 0.002, color: "#ff7edf",  image: pixi, },
+    { size: 2, xp: 18/27, distance: 30, speed: 0.002, color: "#ffffff",  image: postgresql, },
+    { size: 2, xp: 19/27, distance: 30, speed: 0.002, color: "#a2a2a2",  image: prettier, },
+    { size: 2, xp: 20/27, distance: 30, speed: 0.002, color: "#ffffff",  image: react, solid: true },
+    { size: 2, xp: 21/27, distance: 30, speed: 0.002, color: "#00ff23",  image: shadcn, },
+    { size: 2, xp: 22/27, distance: 30, speed: 0.002, color: "#7adcee",  image: sql, },
+    { size: 2, xp: 23/27, distance: 30, speed: 0.002, color: "#7aa8ee",  image: sqlite, },
+    { size: 2, xp: 24/27, distance: 30, speed: 0.002, color: "#ffffff",  image: tailwind, },
+    { size: 2, xp: 25/27, distance: 30, speed: 0.002, color: "#7a5dd1",  image: threejs, },
+    { size: 2, xp: 26/27, distance: 30, speed: 0.002, color: "#0061c0",  image: typescript, },
+    { size: 2, xp: 27/27, distance: 30, speed: 0.002, color: "#ffffff",  image: webpack, solid: true },
+];
+
+
