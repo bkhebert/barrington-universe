1250fdd3fbacff629a74ec7da5d952ba55825bde
bkhebert <bondman2004tippy@gmail.com>
2025-04-09 06:56:15 -0500

    ADD Contact, Projects & Resume components & import into Portfolio component

--- src/client/Portfolio.jsx
+++ src/client/Portfolio.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import AboutMe from "./AboutMe";
 import Skills from "./Skills";
+import Resume from "./Resume";
+import ContactMe from "./ContactMe";
+import Projects from "./Projects";
+
 
 const Portfolio = () => {
 
