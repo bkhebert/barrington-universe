6e59fa09c6709a59d5775f334c3b8cb77e99f617
bkhebert <bondman2004tippy@gmail.com>
2025-04-09 11:00:21 -0500

    ADD conditional rendering

--- src/client/Portfolio.jsx
+++ src/client/Portfolio.jsx
@@ -7,16 +7,40 @@ import Projects from "./Projects";
 
 
 const Portfolio = () => {
+  const [expanded, setExpanded] = useState(null);
+
+  const handleClick = (componentName) => {
+    setExpanded(componentName === expanded ? null : componentName);
+  };
 
   return (
     <div>
-    <div className="bg-red-500/10 min-w-full h-[90rem] flex flex-col">
+    <div className="bg-red-500/10 min-w-full h-[70rem] flex flex-col">
     <div className="flex-grow">nothing</div>
+    { expanded === "Resume" && <Resume></Resume>}
+    { expanded === "Skills" && <Skills></Skills>}
+    { expanded === "Projects" && <Projects></Projects>}
+    { expanded === "AboutMe" && <AboutMe></AboutMe>}
    <div className="grid grid-cols-4">
-    <div className="bg-red-500 h-[26rem] p-4 mb-6">01 Resume</div>
-    <div className="bg-red-500 h-[26rem] p-4 mb-6">02 Skills</div>
-    <div className="bg-red-500 h-[26rem] p-4 mb-6">03 Projects</div>
-    <div className="bg-red-500 h-[26rem] p-4 mb-6">04 About Me</div>
+    <div className="bg-red-500 h-[26rem] p-4 mb-6"
+    onClick={(() => {
+      handleClick("Resume")
+    })}>01 Resume</div>
+    <div className="bg-red-500 h-[26rem] p-4 mb-6"
+    onClick={(() => {
+      handleClick("Skills")
+    })}
+    >02 Skills</div>
+    <div className="bg-red-500 h-[26rem] p-4 mb-6"
+    onClick={(() => {
+      handleClick("Projects")
+    })}
+    >03 Projects</div>
+    <div className="bg-red-500 h-[26rem] p-4 mb-6"
+    onClick={(() => {
+      handleClick("AboutMe")
+    })}
+    >04 About Me</div>
   </div>
   </div>
   </div>
