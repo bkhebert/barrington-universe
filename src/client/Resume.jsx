5439dc88195d4085d3c9208d0d688986acf130dc
bkhebert <bondman2004tippy@gmail.com>
2025-04-10 05:38:43 -0500

    ADD experience & education buttons

--- src/client/Resume.jsx
+++ src/client/Resume.jsx
@@ -100,6 +100,18 @@ const Resume = ({ initialCommand = '', prompt = "user@ubuntu:~$", onCommandSubmi
           <p>Welcome to Ubuntu 20.04.3 LTS (GNU/Linux 5.11.0-43-generic x86_64)</p>
           <p className="mt-1">Last login: {new Date().toLocaleString()}</p>
         </div>
+        <div>
+        <button type="button" class="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        onClick={showExperience}
+        >
+        experience
+        </button>
+        <button type="button" class="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        onClick={showEducation}
+        >
+        education
+        </button>
+        </div>
         
         {/* Command output */}
         {output.map((item, index) => (
