fe2dfff213d1ad5151d2cd9aafb1e07271dd1074
bkhebert <bondman2004tippy@gmail.com>
2025-04-10 06:57:18 -0500

    ADD education Assets

---
+++ src/assets/educationAssets.js
@@ -0,0 +1,27 @@
+export default [
+  
+    {
+      logo: "",
+      title: "University Of New Orleans", 
+      dateOrStack: "August-2015 – Jan 2019 ", 
+      lengthOrDescription: "4 years", 
+      role: "Philosophy B.A", 
+      techStackArray: [],
+      bulletsArray: [
+       "50 Credit Hours towards Computer Science B.S.",
+       "70 Credit Hours towards Physics B.S.",
+      ],
+    },
+    {
+      logo: "",
+      title: "Operation Spark", 
+      dateOrStack: "July-2024 – March 2025", 
+      lengthOrDescription: "9 months", 
+      role: "Advanced Software Engineering Immersive Program", 
+      techStackArray: [  ],
+      bulletsArray: [
+       "IBC: Fundamentals of Javascript, Functional Programming and Web Development Level 3",
+      ],
+    },
+  
+]
