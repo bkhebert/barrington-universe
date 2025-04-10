dcfc38058d34512df2abd66f46612b38c8ed958e
bkhebert <bondman2004tippy@gmail.com>
2025-04-10 06:52:47 -0500

    ADD info for resume assets

--- src/assets/resumeAssets.js
+++ src/assets/resumeAssets.js
@@ -1,9 +1,93 @@
-export default [{
-    image: "",
-    title: "", 
-    dateOrStack: "", 
-    lengthOrDescription: "", 
-    role: "", 
-    techStackArray: [],
-    bulletsArray: [],
-}]
+export default [
+  {
+    logo: "",
+    title: "FLARE", 
+    dateOrStack: "SERN", 
+    lengthOrDescription: "Social-Media Event-Finding App", 
+    role: "Software Engineer", 
+    techStackArray: [ "ExpressJS", "mySQL", "Sequelize", "Socket.io", "PassportJS", "ShadCN", "Tailwind", "Pixi.js", "Jest", "Sequelize", "React", "TypeScript", "Axios", "Magic UI", "Google Gemeni" ],
+    bulletsArray: [
+     "Implemented Tailwind CSS, ShadCN, and Magic UI for consistent design, reusable UI components, and realization of product vision.",
+     "Used TypeScript for code organization & error detection. Tested app features with Jest, ensured error prevention with ESlint.",
+     "Used Webpack for bundling & reduced load time & memory-heap usage by 30% using bundle-analyzer, lazy-loading, & code-splitting.",
+     "Built chatroom feature with real-time multiplayer gaming experience with Socket.io for real-time client to client communication.",
+     "Used Pixi-React beta with React 19 (pre-release), adapting to evolving frameworks still in development w/ little documentation.",
+     "Created seed funcs to seed database with information needed during account creation & signup prior to deployment via AWS.",
+     "Utilized Sequelize ORM for CRUD operations in mySQL, along with Axios for server-to-client communication & RESTful API’s.",
+     "Utilized day.js to provide accurate count-downs and timestamps where necessary, along with async functions for game timers.",
+     "Worked within a team of 5 for a cohesive development process using wireframes, architecture mapping, SCRUM, db diagram & trello."
+    ],
+  },
+  {
+    logo: "",
+    title: "DEMOCRACY ONLINE", 
+    dateOrStack: "SERN", 
+    lengthOrDescription: "AI Logic & Reasonability Checking Debate App!", 
+    role: "Software Engineer", 
+    techStackArray: [ "Postgres", "Express", "React", "Node", "Tailwind", "Axios", "Google Cloud"  ],
+    bulletsArray: [
+     "Created debate forum UI utilizing Tailwind & implemented logic for sending information to database for AI reasonability checks",
+     "Implemented Google API's to get current local politician information based on the users location ",
+     "Made UI & logic for a survey the user would take on signup & used Postgres & Sequelize for storing information on the user",
+    ],
+  },
+  {
+    logo: "",
+    title: "DOGAGATCHI", 
+    dateOrStack: "MERN", 
+    lengthOrDescription: "Similar to Tamagotchi, but with a dog!", 
+    role: "Software Engineer", 
+    techStackArray: [ "Bootstrap", "Babel", "Webpack", "Pixi/React", "MongoDB", "WebPack", "Google Cloud" ],
+    bulletsArray: [
+     "Integrated PixiJS and React-Pixi to generate dynamic graphics and animated sprite characters.",
+     "Implemented JavaScript for game controls, collision detection and physics-based interactions.",
+     "Designed interactive 2D tile-maps using json files generated via SpriteFusion for procedurally generated elements and enemy AI.",
+     "Utilized React-Bootstrap, React-Router-Dom, Node Express, and Axios for dynamic UI updates of player stats.",
+     "Recorded, mixed, & implemented game audio design using Cubase 9 DAW & Free sound libraries.",
+     "Acting Scrum Master who ensured team effectiveness & following of Scrum framework"
+    ],
+  },
+  {
+    logo: "",
+    title: "DIGI-CRY", 
+    dateOrStack: "MERN", 
+    lengthOrDescription: "Mental health App", 
+    role: "Software Engineer", 
+    techStackArray: [ "MongoDB", "Express", "React", "Node", "Google Gemeni API", "Material UI", "Axios", "PassportJS", "Google Cloud"  ],
+    bulletsArray: [
+     "Utilized Google OAuth/Passport.js for seamless authentication and login; tracking user sessions & cookies via Express.",
+     "Implemented Google Gemini API’s Artificial Intelligence, for interactive community forum experience. ",
+     "Used Axios & Node Express for efficient routing & updates within NoSQL database: MongoDB accessed via Mongoose ORM ",
+    ],
+  },
+  {
+    logo: "",
+    title: "UNITED STATES MARINE CORPS ", 
+    dateOrStack: "April-2011 – April-2015 ", 
+    lengthOrDescription: "4 years", 
+    role: "Corporal", 
+    techStackArray: [ "Honor", "Courage", "Commitment", "Discipline", "Tact", "Integrity", "Bearing", "Decisiveness", "Endurance", "Knowledge", "Initiative", "Justice"  ],
+    bulletsArray: [
+      "Served as a leader and mentor, training new Marines in operations and usage of over $70,000,000 of government equipment, discipline, and tactical readiness in high-pressure environments that required adaptability and strategic decision-making.", 
+      "Progressed through the ranks up to Corporal due to leadership, technical expertise, and commitment to excellence. " ,
+      "Held a Secret Security Clearance, demonstrating trustworthiness and the ability to handle sensitive information. ",
+    ],
+  },
+
+  {
+    logo: "",
+    title: "RACCOON CITY MASSACRE & NEW ORLEANS", 
+    dateOrStack: "January 2017 – August 2025 ", 
+    lengthOrDescription: "8 years", 
+    role: "Bartender / Musician", 
+    techStackArray: ["Wix", "Cubase 9", "Avid Pro", "Filmora", "Streamlabs OBS"],
+    bulletsArray: [
+      "Oversaw the production of a series of short-films & music videos which were involved in local film festivals, as well as running post-production using various video-editing software, including Avid Pro, Filmora, & Streamlabs OBS.",
+     "Wrote, recorded, &  produced over 50+ full-length songs using Cubase 9. Taught a team of musicians structure & layout of music.",
+     "Booked, managed, & promoted a nationwide tour across 10 states along with connecting with businesses.",
+     " Developed a band website & brand using Wix that sold over 600$ of band merchandise nation-wide.",
+    ],
+  },
+
+
+]
--- src/client/Experience.jsx
+++ src/client/Experience.jsx
@@ -10,7 +10,7 @@ const Experience = () => {
   return (
     <div>
     <ExperienceCard 
-    image={}
+    logo={}
     title={} 
     dateOrStack={} 
     lengthOrDescription={} 
