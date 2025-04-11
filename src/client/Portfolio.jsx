c0725b6700a03d657b1513174233745c3862d00e
bkhebert <bondman2004tippy@gmail.com>
2025-04-11 13:39:00 -0500

    BUG click events on planets now work, but the html is now off

--- src/client/Portfolio.jsx
+++ src/client/Portfolio.jsx
@@ -87,7 +87,7 @@ const Portfolio = ({itemExpanded}) => {
     <div className={`transition-opacity duration-[3000ms] ease-in ${
         isVisible ? "opacity-100" : "opacity-0"
       }`}>
-    <div className="min-w-full h-screen flex flex-col" >
+    <div className="">
     <div className="flex-grow overflow-hidden flex flex-col min-h-0">
     { expanded === "Resume" && <div className="flex-1 min-h-0 overflow-hidden"> <Resume handleClick={handleClick}></Resume> </div>}
     { expanded === "Skills" && <Skills handleClick={handleClick}></Skills>}
@@ -95,7 +95,7 @@ const Portfolio = ({itemExpanded}) => {
     
         <div
         id="drawer-example"
-        className="fixed top-0 right-0  h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-raccoon-primary/70 w-full dark:bg-gray-800"
+        className="fixed top-0 right-0 z-40 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-raccoon-primary/70 w-full dark:bg-gray-800"
         tabIndex="-1"
         aria-labelledby="drawer-label"
       >
