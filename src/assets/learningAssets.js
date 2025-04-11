8f99dbe708b8f566995b464874469af9bd5f1213
bkhebert <bondman2004tippy@gmail.com>
2025-04-11 03:47:47 -0500

    ADD learning assets

---
+++ src/assets/learningAssets.js
@@ -0,0 +1,11 @@
+import Csharpnoskew from "./UnskewedLogos/Csharpnoskew.png"
+import vimnoskew from "./UnskewedLogos/vimnoskew.png"
+import dockernoskew from "./UnskewedLogos/dockernoskew.png"
+import dotnetnoskew from "./UnskewedLogos/dotnetnoskew.png"
+
+export default [
+  { color: "#FFFFFF",  image: Csharpnoskew, },
+  { color: "#FFFFFF",  image: vimnoskew, },
+  { color: "#FFFFFF",  image: dockernoskew, },
+  { color: "#FFFFFF",  image: dotnetnoskew, },
+]
--- src/client/Skills.jsx
+++ src/client/Skills.jsx
@@ -7,10 +7,10 @@ export default function Skills({handleClick}) {
 
   return (
 
-<div class="overflow-y-auto overflow-x-hidden z-10 fixed top-0 right-0 left-0 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-opacity-50 backdrop-blur-sm">
-    <div class="relative p-4 w-full max-w-2xl max-h-full">
+    <div className="fixed inset-0 z-50 flex items-center justify-center p-4 bg-black bg-opacity-50 backdrop-blur-sm">
+      <div className="relative w-full max-w-2xl max-h-full overflow-y-auto">
        
-        <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
+        <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700  bg-neutral-300">
           
             <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
                 <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
