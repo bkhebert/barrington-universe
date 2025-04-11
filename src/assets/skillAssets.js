372e25ec4060a22400e11a687a069bd90556b66f
bkhebert <bondman2004tippy@gmail.com>
2025-04-11 03:00:17 -0500

    ADD unskew logos

---
+++ src/assets/skillAssets.js
@@ -0,0 +1,59 @@
+import skewedjsnoskew from "../assets/skewedjsnoskew.png";
+import angularnoskew from "../assets/skewedlogos/angularnoskew.png"
+import cssnoskew from "../assets/skewedlogos/cssnoskew.png"
+import digitaloceannoskew from "../assets/skewedlogos/digitaloceannoskew.png"
+import expressjsnoskew from "../assets/skewedlogos/express-jsnoskew.png"
+import githubnoskew from "../assets/skewedlogos/githubnoskew.png"
+import googlecloudnoskew from "../assets/skewedlogos/googlecloudnoskew.png"
+import HTML5noskew from "../assets/skewedlogos/HTML5noskew.png"
+import linuxnoskew from "../assets/skewedlogos/linuxnoskew.png"
+import magicuinoskew from "../assets/skewedlogos/magicuinoskew.png"
+import materialuinoskew from "../assets/skewedlogos/materialuinoskew.png"
+import mongodbnoskew from "../assets/skewedlogos/mongodbnoskew.png"
+import mysqlnoskew from "../assets/skewedlogos/mysqlnoskew.png"
+import nginxnoskew from "../assets/skewedlogos/nginxnoskew.png"
+import nodenoskew from "../assets/skewedlogos/nodenoskew.png"
+import npmnoskew from "../assets/skewedlogos/npmnoskew.png"
+import pixinoskew from "../assets/skewedlogos/pixinoskew.png"
+import postgresqlnoskew from "../assets/skewedlogos/postgresqlnoskew.png"
+import prettiernoskew from "../assets/skewedlogos/prettiernoskew.png"
+import reactnoskew from "../assets/skewedlogos/reactnoskew.png"
+import shadcnnoskew from "../assets/skewedlogos/shadcnnoskew.png"
+import sqlnoskew from "../assets/skewedlogos/SQLnoskew.png"
+import sqlitenoskew from "../assets/skewedlogos/sqlitenoskew.png"
+import tailwindnoskew from "../assets/skewedlogos/tailwindnoskew.png"
+import threejsnoskew from "../assets/skewedlogos/threejsnoskew.png"
+import typescriptnoskew from "../assets/skewedlogos/typescriptnoskew.png"
+import webpacknoskew from "../assets/skewedlogos/webpacknoskew.png"
+
+export default [
+    { color: "#f4ec0f",  image: skewedjsnoskew, },
+    { color: "#eda100",  image: angularnoskew, },
+    { color: "#0fd6f4",  image: cssnoskew, },
+    { color: "#005fff",  image: digitaloceannoskew, },
+    { color: "#25f900",  image: expressjsnoskew, },
+    { color: "#ffffff",  image: githubnoskew, },
+    { color: "#000000",  image: googlecloudnoskew, },
+    { color: "#f4810f",  image: HTML5noskew, },
+    { color: "#504e42",  image: linuxnoskew, },
+    { color: "#6800ff",  image: magicuinoskew, },
+    { color: "#ffffff",  image: materialuinoskew, },
+    { color: "#60ef61",  image: mongodbnoskew, },
+    { color: "#97e7ff",  image: mysqlnoskew, },
+    { color: "#005e19",  image: nginxnoskew, },
+    { color: "#ffffff",  image: nodenoskew, },
+    { color: "#c20000",  image: npmnoskew, },
+    { color: "#ff7edf",  image: pixinoskew, },
+    { color: "#ffffff",  image: postgresqlnoskew, },
+    { color: "#a2a2a2",  image: prettiernoskew, },
+    { color: "#ffffff",  image: reactnoskew },
+    { color: "#00ff23",  image: shadcnnoskew, },
+    { color: "#7adcee",  image: sqlnoskew, },
+    { color: "#7aa8ee",  image: sqlitenoskew, },
+    { color: "#ffffff",  image: tailwindnoskew, },
+    { color: "#7a5dd1",  image: threejsnoskew, },
+    { color: "#0061c0",  image: typescriptnoskew, },
+    { color: "#ffffff",  image: webpacknoskew },
+];
+
+
--- src/client/Skills.jsx
+++ src/client/Skills.jsx
@@ -1,19 +1,20 @@
 import React, { useEffect } from 'react';
 import { initDrawers } from 'flowbite';
+import planetAssets from '../assets/planetAssets';
 
 export default function Skills({handleClick}) {
 
 
   return (
 
-<div class="overflow-y-auto overflow-x-hidden z-10 fixed top-0 right-0 left-0 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+<div class="overflow-y-auto overflow-x-hidden z-10 fixed top-0 right-0 left-0 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-opacity-50 backdrop-blur-sm">
     <div class="relative p-4 w-full max-w-2xl max-h-full">
        
         <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
           
             <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
                 <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                    Terms of Service
+                    Skills
                 </h3>
                 <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
                  onClick={(() => {
@@ -23,17 +24,14 @@ export default function Skills({handleClick}) {
                     <svg class="w-3 h-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                     </svg>
-                    <span class="sr-only">Close modal</span>
+                  
                 </button>
             </div>
           
             <div class="p-4 md:p-5 space-y-4">
-                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                    With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
-                </p>
-                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                    The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-                </p>
+                { planetAssets.map((skill) => (
+                  <img class={`w-[100px] h-[60px] rounded-lg overflow-hidden`} style={{backgroundColor: `${skill.color}`}} src={skill.image} alt="Default avatar"></img>
+                ))}
             </div>
         
             <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
